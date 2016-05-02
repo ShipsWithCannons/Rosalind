e17b39b2eb792681195d83f1eb77a6217bb08339
- <->
2016-05-02 12:49:54 +0200

    Added solution to Rabbits and Recurrence Relations

---
+++ Ruby/FIB.rb
@@ -0,0 +1,13 @@
+def wabbits(n, k)
+    fibs = 1, 1, k + 1
+    4.upto n do |x|
+    fibs[0] = fibs[1]
+    fibs[1] = fibs[2]
+    fibs[2] = fibs[0] * k + fibs[1]
+    end
+    return fibs[2]
+end
+
+user_input = gets.chomp
+split_input = user_input.split().map { |number| number.to_i() }
+puts wabbits(split_input[0], split_input[1])
