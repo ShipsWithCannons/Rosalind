426dbf05471385b1bed94ee4426acce49edd5a2c
ShipsWithCannons <alwayscomps@hotmail.com>
2016-06-05 22:43:49 +0200

    Added 1 raising and 3 succeeding expectations

--- Ruby/tst/Validation_unittest.rb
+++ Ruby/tst/Validation_unittest.rb
@@ -8,3 +8,27 @@ describe "Size verification with empty right" do
     expect { v.EqualSize([1], []) }.to raise_error(ArgumentError)
   end
 end
+
+describe "Size verification with left right" do
+  it "raises" do
+    expect { v.EqualSize([], [1]) }.to raise_error(ArgumentError)
+  end
+end
+
+describe "Size verification with both empty" do
+  it "succeeds" do
+    expect { v.EqualSize([], []) }.not_to raise_error
+  end
+end
+
+describe "Size verification with size 1, different values" do
+  it "succeeds" do
+    expect { v.EqualSize([1], [0]) }.not_to raise_error
+  end
+end
+
+describe "Size verification with both size 1, same values" do
+  it "succeeds" do
+    expect { v.EqualSize([1], [1]) }.not_to raise_error
+  end
+end
