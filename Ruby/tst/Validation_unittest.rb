ebc9286863f09285d5f269a8ea1d89e769b75bfa
ShipsWithCannons <alwayscomps@hotmail.com>
2016-06-05 22:37:46 +0200

    Fix blatant error in the unittest.

--- Ruby/tst/Validation_unittest.rb
+++ Ruby/tst/Validation_unittest.rb
@@ -1,6 +1,10 @@
 require 'rspec'
 require_relative '../lib/Validation'
+
 v = Validation.new()
-describe 'Size verification with empty right' do
-  it { expect { v.EqualSize([1], [1]) }.to raise_error }
+
+describe "Size verification with empty right" do
+  it "raises" do
+    expect { v.EqualSize([1], []) }.to raise_error(ArgumentError)
+  end
 end
